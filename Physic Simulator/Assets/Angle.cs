a15fce216e5e9d24f72acf97636009f14cccfce9
NanoCode012 <kevinvong@rocketmail.com>
2017-10-02 17:06:48 +0700

    Added Physic Simulator
    Added project Physic Simulator. Ongoing Project.

---
+++ Physic Simulator/Assets/Angle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Angle : MonoBehaviour {
+    
+    RectTransform rectTransform;
+    // Use this for initialization
+    void Start () {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void AdjustRotation(float angle)
+    {
+		rectTransform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    public void AdjustSize(float power)
+    {
+        rectTransform.localScale = new Vector3(power, rectTransform.localScale.y, rectTransform.localScale.z);
+    }
+}
+
