5d89b1541139303b195dd9f6b0b2f40bef70a091
NanoCode012 <kevinvong@rocketmail.com>
2017-10-02 23:46:17 +0700

    Added a way to move ship through mouse. Improved performance by caching Camera.
    Implemented a way to test/debug the game through mouse instead of the slow arrow keys. Can also be considered as a 'cheat'.
    Cached the Camera into mainCamera, so it doesn't have to search for MainCamera tag each time Camera is used.

--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -11,17 +11,24 @@ public class PlayerController : MonoBehaviour {
     float xMax;
     float yMin;
     float yMax;
+    public bool useMouse;
+    Camera mainCamera = new Camera();
  
 	void Start () {
+        mainCamera = Camera.main;
         //distance from player to camera
-        var distance = transform.position.z - Camera.main.transform.position.z;
+        var distance = transform.position.z - mainCamera.transform.position.z;
 
-        //viewport(0,0) is bottom-left, (1,1) is top-right
-        var bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
-        var topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));
+		//viewport(0,0) is bottom-left, (1,1) is top-right
+        var bottomLeft = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		var topRight = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, distance));
+		//commented out works too, this is just to try different ways of doing the same thing
+		//var bottomLeft = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, distance));
+		//var topRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, distance));
 
-        //automatically set values independant of screen size
-        xMin = bottomLeft.x + padding;
+
+		//automatically set values independant of screen size
+		xMin = bottomLeft.x + padding;
         xMax = topRight.x - padding;
         yMin = bottomLeft.y + padding;
         yMax = topRight.y - padding;
@@ -29,28 +36,39 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate()//for smoother movement, 0.02s per frame I believe
 	{
-        //allows diagonal movement
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.position -= new Vector3(x_speed * Time.deltaTime, 0f, 0f);//moves x_speed per sec
-		}
-		else if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.position += new Vector3(x_speed * Time.deltaTime, 0f, 0f);
-		}
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
-			transform.position += new Vector3(0f, y_speed * Time.deltaTime, 0f);//moves y_speed per sec
-		}
-		else if (Input.GetKey(KeyCode.DownArrow))
-		{
-			transform.position -= new Vector3(0f, y_speed * Time.deltaTime, 0f);
-		}
+        
+        //allows mouse control
+        if (useMouse)
+        {
+            var mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);//Screen is GUI
+            var posX = Mathf.Clamp(mousePos.x, xMin, xMax);
+            var posY = Mathf.Clamp(mousePos.y, yMin, yMax);
+            transform.position = new Vector3(posX, posY, transform.position.z);
+        }
+        else
+        {
+			//allows diagonal movement
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				transform.position -= new Vector3(x_speed * Time.deltaTime, 0f, 0f);//moves x_speed per sec
+			}
+			else if (Input.GetKey(KeyCode.RightArrow))
+			{
+				transform.position += new Vector3(x_speed * Time.deltaTime, 0f, 0f);
+			}
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				transform.position += new Vector3(0f, y_speed * Time.deltaTime, 0f);//moves y_speed per sec
+			}
+			else if (Input.GetKey(KeyCode.DownArrow))
+			{
+				transform.position -= new Vector3(0f, y_speed * Time.deltaTime, 0f);
+			}
 
-        //clamps player inside screen
-		var newX = Mathf.Clamp(transform.position.x, xMin, xMax);
-        var newY = Mathf.Clamp(transform.position.y, yMin, yMax);
-        transform.position = new Vector3(newX, newY, transform.position.z);
-
+			//clamps player inside screen
+			var newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+			var newY = Mathf.Clamp(transform.position.y, yMin, yMax);
+			transform.position = new Vector3(newX, newY, transform.position.z);
+        }
 	}
 }
