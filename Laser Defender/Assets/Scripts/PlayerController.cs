71588b55c0a622a2d6c5b5cf2ff11b2062c07e80
NanoCode012 <kevinvong@rocketmail.com>
2017-10-02 23:04:50 +0700

    Moved Input control from Update() to FixedUpdate()
    Moved Input code to FixedUpdate because Time.deltaTime is the same through out, allowing for more smoother movement each second and between frames.
    Clamped player movement within screen. Added code to detect the world units of the screen edges respective to screen resolution, therefore support for more resolution, however padding from edges is constant.
    Added more comments to clearly explain code and kept it evenly spaced out for easy readabiliy.

--- Laser Defender/Assets/Scripts/PlayerController.cs
+++ Laser Defender/Assets/Scripts/PlayerController.cs
@@ -4,51 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float x_speed = 3.5f;
-    public float y_speed = 1.8f;
-	// Use this for initialization
+    float x_speed = 3.7f;
+    float y_speed = 2.3f;
+    float padding = 0.6f;
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+ 
 	void Start () {
-		
-	}
-	
-    /*
-	void Update () {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position -= new Vector3(x_speed * Time.deltaTime, 0f, 0f);//moves x_speed per sec
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += new Vector3(x_speed * Time.deltaTime, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += new Vector3(0f, y_speed * Time.deltaTime, 0f);//moves y_speed per sec
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position -= new Vector3(0f, y_speed * Time.deltaTime, 0f);
-        }
-	}
-	*/
+        //distance from player to camera
+        var distance = transform.position.z - Camera.main.transform.position.z;
 
-	void FixedUpdate()
+        //viewport(0,0) is bottom-left, (1,1) is top-right
+        var bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        var topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));
+
+        //automatically set values independant of screen size
+        xMin = bottomLeft.x + padding;
+        xMax = topRight.x - padding;
+        yMin = bottomLeft.y + padding;
+        yMax = topRight.y - padding;
+	}
+
+	void FixedUpdate()//for smoother movement, 0.02s per frame I believe
 	{
+        //allows diagonal movement
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			transform.position -= new Vector3(x_speed, 0f, 0f);
+			transform.position -= new Vector3(x_speed * Time.deltaTime, 0f, 0f);//moves x_speed per sec
 		}
 		else if (Input.GetKey(KeyCode.RightArrow))
 		{
-			transform.position += new Vector3(x_speed, 0f, 0f);
+			transform.position += new Vector3(x_speed * Time.deltaTime, 0f, 0f);
 		}
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
-			transform.position += new Vector3(0f, y_speed, 0f);
+			transform.position += new Vector3(0f, y_speed * Time.deltaTime, 0f);//moves y_speed per sec
 		}
 		else if (Input.GetKey(KeyCode.DownArrow))
 		{
-			transform.position -= new Vector3(0f, y_speed, 0f);
+			transform.position -= new Vector3(0f, y_speed * Time.deltaTime, 0f);
 		}
+
+        //clamps player inside screen
+		var newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+        var newY = Mathf.Clamp(transform.position.y, yMin, yMax);
+        transform.position = new Vector3(newX, newY, transform.position.z);
+
 	}
 }
