380c8042e24de4698fa10087816eb9cc2b44eb7e
NanoCode012 <kevinvong@rocketmail.com>
2017-10-03 17:27:37 +0700

    Tidied up scripts folder. Can now spawn enemy at gameObject Position
    Organized scripts folder. Enemy SpaceShips spawn at gameObject Position. Position has now OnDrawGizmo to show its position for easy editing in UnityEditor, will not affect Game.

---
+++ Laser Defender/Assets/Scripts/Main/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+	// Use this for initialization
+	void Start () {
+        foreach (Transform transf in transform)
+        {
+            var enemy = Instantiate(enemyPrefab, transf.position, Quaternion.identity);
+            enemy.transform.parent = transf;
+        }
+
+        //Another method could be done using the below(I made the below, but it takes twice as long to process)
+		//var enemyPostions = FindObjectsOfType<Position>();
+		//foreach (var pos in enemyPostions)
+		//{
+		//	var enemy = Instantiate(enemyPrefab, pos.transform.position, Quaternion.identity);
+		//	enemy.transform.parent = pos.transform;
+		//}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Laser Defender/Assets/Scripts/Main/Position.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour {
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 0.35f);
+    }
+}
