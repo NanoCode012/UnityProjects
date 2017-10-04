b810e27d9919f7b3fa4ee4762c01c592f5c9ebf3
NanoCode012 <kevinvong@rocketmail.com>
2017-10-04 15:21:23 +0700

    Added a shredder. Added a collider for the acidball
    Added Shredders that basically shreds acidballs which leaves the gameSpace. AcidBall gets destroyed when touches Shredder.

---
+++ Laser Defender/Assets/Scripts/Misc/Shredder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+	
+    void OnTriggerEnter2D(Collider2D col) {
+        Destroy(col.gameObject);
+	}
+}
