a9b673d9fa533a1e79c398951adb2d13f6f9e693
NanoCode012 <kevinvong@rocketmail.com>
2017-11-22 10:39:28 +0700

    Added a FadeIn Panel UI to the Start Screen
    Also moved scripts around to their proper folders.

---
+++ Glitch Garden/Assets/Scripts/Misc/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    public float fadeTime = 2;
+    Image panel;
+    Color currentCol = Color.black;
+	void Start () {
+        panel = GetComponent<Image>();
+        currentCol.a = 1f;
+        panel.color = currentCol;
+	}
+	
+	void Update () {
+        if (Time.timeSinceLevelLoad < fadeTime){
+            Fade();
+        }else{
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Fade(){
+        currentCol.a -= Time.deltaTime / fadeTime;
+        panel.color = currentCol;
+    }
+}
