84466b5a400a1297f55e033f9798b1a714daf518
NanoCode012 <kevinvong@rocketmail.com>
2017-11-25 21:46:27 +0700

    Made a 'wrapper' for PlayerPrefs to centralize all calls.
    This way have to call PlayerPrefsManager to set value in PlayerPrefs, and we will not need to remember the specific key string. Added a few simple checks in case  an invalid value is set.

---
+++ Glitch Garden/Assets/Scripts/Misc/PlayerPrefsManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPrefsManager : MonoBehaviour {
+
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+	
+
+    public static void SetMasterVolume(float volume){
+        if (0f <= volume && volume <= 1f){
+			PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }else{
+            Debug.LogError("Volume cannot be set out of range. Only within 0->1f. You input " + volume);
+        }
+    }
+
+    public static float GetMasterVolume(){
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    //
+
+    public static void UnlockLevel(int level){
+        if (level <= SceneManager.sceneCountInBuildSettings - 1) {
+            PlayerPrefs.SetInt(LEVEL_KEY + level, 1);//1 true, 0 false
+        }
+        else {
+            Debug.LogError("Cannot unlockLevel higher than count of scenes. Only within 0->sceneCount - 1. You input " + level);
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level){
+        if (level <= SceneManager.sceneCountInBuildSettings - 1) {
+			return (PlayerPrefs.GetInt(LEVEL_KEY + level) == 1);
+        }else {
+            Debug.LogError("Cannot check levelUnlock on scenes higher than count of scenes. Only within 0->sceneCount - 1. You input " + level);
+            return false;
+        }
+    }
+
+    //
+
+    public static void SetDifficulty(int difficulty){
+        if (0 <= difficulty && difficulty <= 2) {
+            PlayerPrefs.SetInt(DIFFICULTY_KEY, difficulty);
+        }else {
+            Debug.LogError("Difficulty cannot be set out of range. Only within 0->2. You input " + difficulty);//0 easy, 1 normal, 2 hard
+        }
+    }
+
+    public static int GetDifficulty(){
+        return PlayerPrefs.GetInt(DIFFICULTY_KEY);
+    }
+}
