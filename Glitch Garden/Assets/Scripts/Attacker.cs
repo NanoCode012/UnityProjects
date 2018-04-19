46c88bf3056e58cb1fb60e072e5f233fbd7cb9bc
NanoCode012 <kevinvong@rocketmail.com>
2018-04-19 15:34:10 +0700

    Added trigger detection and some minor animation changes

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(0f, 3f)]
+    public float walkSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+        rigidbody2D.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+}
---
+++ Glitch Garden/Assets/Scripts/Projectiles.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        print(name + " is touched by " + collision.name);
+	}
+}
