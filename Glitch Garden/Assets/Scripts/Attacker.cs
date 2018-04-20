9bcb59fe55779a5cba6bb64ccaa5772ac54df2be
NanoCode012 <kevinvong@rocketmail.com>
2018-04-20 11:53:45 +0700

    Added Animation events for Attackers. Renamed fox appear to run. Removed fox idle. Added fox jump
    Added a test StrikeDamage method for attackers to emulate dealing damage

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class Attacker : MonoBehaviour {
 
     [Range(0f, 3f)]
-    public float walkSpeed;
-
+    private float currentSpeed;
 
 	// Use this for initialization
 	void Start () {
-        Rigidbody2D rigidbody2D = gameObject.AddComponent<Rigidbody2D>();
-        rigidbody2D.isKinematic = true;
+        Rigidbody2D myRigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody2D.isKinematic = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector3.left * currentSpeed * Time.deltaTime);
 	}
+
+    public void SetSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+    public void StrikeCurrentTarget(float damage)
+    {
+        print(name + " dealt " + damage + " damage");
+    }
 }
