f9a0d3b14b5e1248fd13fd2bf48a1d7e00264a02
NanoCode012 <kevinvong@rocketmail.com>
2018-04-24 16:51:49 +0700

    Moved projectiles to its own prefab. Fixed defenders animations to not change state until attacked

---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour {
+
+    public float speed;
+    public float damage;
+    Rigidbody2D myRigidbody2D;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        print(name + " is touched by " + collision.name);
+	}
+
+	private void Start()
+	{
+        myRigidbody2D = gameObject.AddComponent<Rigidbody2D>();
+        myRigidbody2D.isKinematic = true;
+	}
+
+	private void Update()
+	{
+        myRigidbody2D.transform.Translate(Vector3.right * speed * Time.deltaTime);
+	}
+}
