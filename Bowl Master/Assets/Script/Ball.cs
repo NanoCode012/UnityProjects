a3b9472951666937c2a0398257f2668ea2c21198
NanoCode012 <kevinvong@rocketmail.com>
2018-06-14 14:49:05 +0700

    Added Ball and Camera Tracking Scripts
    Camera now follow ball

---
+++ Bowl Master/Assets/Script/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+public class Ball : MonoBehaviour {
+
+    public AudioClip rollingClip;
+
+    [SerializeField] private float initialVelocity = 3f;
+    private Rigidbody myRigidBody;
+    private AudioSource audioSource;
+
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+        myRigidBody.velocity = Vector3.forward * (initialVelocity);
+
+        if (rollingClip == null) Debug.LogWarning("Ball's rollingClip is missing!");
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = rollingClip;
+        audioSource.loop = true;
+        audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Bowl Master/Assets/Script/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Ball ball;
+
+    private Vector3 offset;
+    private const float positionOfFirstPin = 1829f;
+    private readonly float positionToStopAt = positionOfFirstPin - 100;
+
+	// Use this for initialization
+	void Start () {
+        offset = ball.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z < positionToStopAt)
+        {
+            transform.position = ball.transform.position - offset;
+        }
+	}
+}
