4b8fa0834514c2bc26aa40edc35f4c2b1a8c2cd0
NanoCode012 <kevinvong@rocketmail.com>
2018-06-16 10:25:28 +0700

    Added a Touch Control System to move ball

--- Bowl Master/Assets/Script/Ball.cs
+++ Bowl Master/Assets/Script/Ball.cs
@@ -3,28 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent (typeof(AudioSource))]
+[RequireComponent (typeof(Rigidbody))]
 public class Ball : MonoBehaviour {
 
     public AudioClip rollingClip;
 
-    [SerializeField] private float initialVelocity = 3f;
     private Rigidbody myRigidBody;
     private AudioSource audioSource;
 
 	void Start () {
+
         myRigidBody = GetComponent<Rigidbody>();
-        myRigidBody.velocity = Vector3.forward * (initialVelocity);
+        audioSource = GetComponent<AudioSource>();
+
+        myRigidBody.useGravity = false;
 
         if (rollingClip == null) Debug.LogWarning("Ball's rollingClip is missing!");
+        else
+        { 
+            audioSource.clip = rollingClip;
+            audioSource.loop = true;
+        }
+    }
 
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = rollingClip;
-        audioSource.loop = true;
+    public void Launch(Vector3 velocity)
+    {
+        myRigidBody.velocity = velocity;
+        myRigidBody.useGravity = true;
+
         audioSource.Play();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
---
+++ Bowl Master/Assets/Script/DragLaunch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class DragLaunch : MonoBehaviour {
+
+    private Ball ball;
+
+    private Vector3 startPos;
+    private float startTime;
+
+    // Use this for initialization
+    void Start () {
+        ball = GetComponent<Ball>();
+	}
+
+    public void DragStart()
+    {
+        startPos = Input.mousePosition;
+        startTime = Time.realtimeSinceStartup;
+    }
+
+    public void DragEnd()
+    {
+        Vector3 endPos = Input.mousePosition;
+        Vector3 offset = endPos - startPos;
+
+        float endTime = Time.realtimeSinceStartup;
+        float timeTaken = endTime - startTime;
+
+        ball.Launch(new Vector3(offset.x, 0, offset.y) / timeTaken);
+    }
+
+}
+
+
