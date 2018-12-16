d7bbab7028cb278d0a21f94f08730927b39b5407
NanoCode012 <kevinvong@rocketmail.com>
2018-12-16 15:31:47 +0700

    Added a UnityTest for ActionMaster

---
+++ Bowl Master/Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class ActionMasterTest
+{
+    private readonly ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+    [Test]
+    public void T00PassingTest()
+    {
+        Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01OneStrikeReturnsEndTurn()
+    {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+}
---
+++ Bowl Master/Assets/Script/ActionMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster
+{
+
+    public enum Action
+    {
+        Tidy,
+        Reset,
+        EndTurn,
+        EndGame
+    };
+
+    public Action Bowl(int pins)
+    {
+        if (pins < 0 || pins > 10) throw new UnityException("Invalid pins");
+
+        if (pins == 10) return Action.EndTurn;
+
+        throw new UnityException("No action returned");
+    }
+}
