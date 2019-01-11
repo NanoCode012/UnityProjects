940de3b805f2c813e3593ebba38dbcf609488d2a
NanoCode012 <kevinvong@rocketmail.com>
2019-01-11 15:50:25 +0700

    Added ScoreMaster and ScoreMasterTest

---
+++ Bowl Master/Assets/Script/ScoreMaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMaster
+{
+    public static List<int> GetScoreFrames(List<int> rolls)
+    {
+        var scoreFrames = new List<int>();
+
+        return scoreFrames;
+    }
+
+    public static List<int> GetScoreCumulative(List<int> rolls)
+    {
+        var scoreCumulative = new List<int>();
+
+        var sum = 0;
+        var scoreFrames = GetScoreFrames(rolls);
+
+        foreach (var scoreFrame in scoreFrames)
+        {
+            sum += scoreFrame;
+            scoreCumulative.Add(sum);
+        }
+
+        return scoreCumulative;
+    }
+	
+
+}
