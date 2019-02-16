3f6fcf6e5541d3fa78e2194ace731805fd726bf9
NanoCode012 <kevinvong@rocketmail.com>
2019-02-16 10:10:36 +0700

    Added ScoreDisplay and wired it to Texts

---
+++ Bowl Master/Assets/Script/ScoreDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] rollTexts;
+    public Text[] frameTexts;
+
+	// Use this for initialization
+	void Start () {
+		foreach(var text in rollTexts)
+        {
+            text.text = "";
+        }
+        foreach (var text in frameTexts)
+        {
+            text.text = "";
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
