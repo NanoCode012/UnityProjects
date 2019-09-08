1f51a7f88d0f3ea2a45a1f9ab3679fbd02eb7bf9
NanoCode012 <kevinvong@rocketmail.com>
2019-09-08 16:12:32 +0700

    Added Playerscript to Respawn at set positions

---
+++ Zombie Runner/Assets/Script/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject SpawnPointParent;
+
+    [SerializeField] private bool callRespawn = false;
+
+    private Transform playerTransform;
+    private Transform[] spawnPoints;
+    private readonly System.Random rand = new System.Random();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = SpawnPointParent.GetComponentsInChildren<Transform>();
+        playerTransform = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (callRespawn)
+        {
+            Respawn();
+            callRespawn = false;
+        }
+    }
+
+    void Respawn()
+    {
+        playerTransform.position = spawnPoints[rand.Next(spawnPoints.Length)].position;
+    }
+}
