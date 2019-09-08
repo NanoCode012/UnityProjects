afefea19ef7e3fdd33e9b82e16d75132dd51bf62
NanoCode012 <kevinvong@rocketmail.com>
2019-09-08 22:38:03 +0700

    Add a Daycycle feature

---
+++ Zombie Runner/Assets/Daycycle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Daycycle : MonoBehaviour
+{
+
+    public float timeScale = 60;
+
+    public bool isDay = true;
+    float increment;
+
+    private Quaternion startRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        increment =  360 / timeScale;
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (isDay) transform.eulerAngles = new Vector3(30, -30, 0);
+        //else transform.eulerAngles = new Vector3(220, -30, 0);
+
+        //0->86400 s/day
+        //0->360 degree
+        //transform.eulerAngles = new Vector3(transform.eulerAngles.x + increment/Time.deltaTime, transform.eulerAngles.y, transform.eulerAngles.z);
+        transform.RotateAround(transform.position, Vector3.forward, Time.deltaTime / increment);
+    }
+}
