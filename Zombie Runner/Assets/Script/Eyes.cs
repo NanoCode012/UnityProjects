4f809717d6102b6e557d3c66b88590fcec01ad42
NanoCode012 <kevinvong@rocketmail.com>
2019-09-08 17:45:02 +0700

    Added Zoom feature

---
+++ Zombie Runner/Assets/Script/Eyes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour
+{
+    private Camera camera;
+    private float defaultFOV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        defaultFOV = camera.fieldOfView;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Zoom"))
+        {
+            camera.fieldOfView /= 1.5f;
+        }
+        else if (Input.GetButtonUp("Zoom"))
+        {
+            camera.fieldOfView = defaultFOV;
+        }
+    }
+
+    
+}
